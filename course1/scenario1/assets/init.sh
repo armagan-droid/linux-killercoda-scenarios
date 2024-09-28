197eafbc7263d8109f5d480c39c3d06226c63e2e
armağan yaman <yaman.armagan@gmail.com>
2024-09-28 21:41:58 +0300

    init script edited

--- course1/scenario1/assets/init.sh
+++ course1/scenario1/assets/init.sh
@@ -6,7 +6,7 @@ mkdir -p /opt/files
 mkdir -p /opt/bakfiles
 mkdir -p /opt/logs
 mkdir -p /opt/step1
-touch /opt/files/file{1..3}
+touch /opt/files/file{1..3}.txt
 
 # mark init finished
 touch /opt/.initfinished
